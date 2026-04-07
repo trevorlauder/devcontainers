6e88e60109db14c2b3748a10a2ad5e43c1f59f7e
Trevor Lauder <trevor.l@getjobber.com>
2026-04-07 16:00:04 -0600

    Update install.sh
    Signed-off-by: Trevor Lauder <trevor.l@getjobber.com>

--- src/ansible/install.sh
+++ src/ansible/install.sh
@@ -7,4 +7,4 @@ if [ "${ENABLEFIREWALL}" = "true" ]; then
     install -m 0644 "${FEATURE_DIR}/firewall-fqdns.txt" /usr/local/etc/firewall-extra-fqdns.d/feature-ansible-fqdns.txt
 fi
 
-install -o "${USERNAME}" -g "${USERNAME}" -m 0644 "${FEATURE_DIR}/mise.toml" "/home/${USERNAME}/.config/mise/conf.d/130-ansible.toml"
+install -o "${USERNAME}" -g "${USERNAME}" -m 0644 "${FEATURE_DIR}/mise.toml" "/home/${USERNAME}/.config/mise/conf.d/979-ansible.toml"
