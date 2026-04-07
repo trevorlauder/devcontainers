f32b9678c521abc7362e88cd2937e16b305ac977
Trevor Lauder <trevor.l@getjobber.com>
2026-04-07 16:00:44 -0600

    Update install.sh
    Signed-off-by: Trevor Lauder <trevor.l@getjobber.com>

--- src/python/install.sh
+++ src/python/install.sh
@@ -10,4 +10,4 @@ fi
 mkdir -p "/home/${USERNAME}/.local/share/uv"
 chown "${USERNAME}:${USERNAME}" "/home/${USERNAME}/.local/share/uv"
 
-install -o "${USERNAME}" -g "${USERNAME}" -m 0644 "${FEATURE_DIR}/mise.toml" "/home/${USERNAME}/.config/mise/conf.d/120-python.toml"
+install -o "${USERNAME}" -g "${USERNAME}" -m 0644 "${FEATURE_DIR}/mise.toml" "/home/${USERNAME}/.config/mise/conf.d/979-python.toml"
